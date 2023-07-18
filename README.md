9dc7c298bcd2d9869534302fa4eeb44f5dd0ac05
mddevils <fabiodevil@gmail.com>
2023-07-18 17:26:22 -0300

    new file:   .editorconfig 	new file:   .eslintignore 	new file:   .eslintrc 	new file:   .npmrc 	new file:   README.md 	new file:   esbuild.config.mjs 	new file:   main.ts 	new file:   manifest.json 	new file:   package-lock.json 	new file:   package.json 	new file:   settings.png 	new file:   tsconfig.json 	new file:   version-bump.mjs 	new file:   versions.json

---
+++ README.md
@@ -0,0 +1,31 @@
+# Obsidian Floccus Bookmarks Plugin
+
+Obsidian Floccus Bookmarks is a plugin for [Obsidian](https://obsidian.md/) that allows you to import and convert bookmarks from an XBEL file into a Markdown file. This plugin is inspired by [Floccus](https://floccus.org/), a browser extension that synchronizes bookmarks across different browsers.
+
+## Installation
+
+1. Download the plugin files and place them in your Obsidian vault's plugins folder.
+2. Enable the plugin in Obsidian's settings.
+
+## Usage
+
+1. Configure the plugin settings in the Obsidian settings menu under "Obsidian Floccus Bookmark Settings".
+2. In the left ribbon of Obsidian, click on the bookmark icon to import and convert the bookmarks from the XBEL file.
+3. The generated Markdown file will be created in the specified folder path with the specified filename.
+
+## Plugin Settings
+
+The plugin settings can be accessed in the Obsidian settings menu under "Obsidian Floccus Bookmark Settings". The following settings are available:
+
+- **XBEL Absolute Folder Path**: The absolute folder path of the XBEL file.
+- **XBEL Filename**: The filename of the XBEL file.
+- **MD Vault Folder Path**: The vault folder for the generated Markdown file.
+- **MD Filename**: The filename for the generated Markdown file.
+- **Backup Folder Path**: The folder path for the backup files.
+- **Number of Backups to Keep**: The number of backup files to keep.
+- **Automatic Update Bookmarks**: Enable automatic updating of bookmarks.
+- **Update Interval (in seconds)**: Specify the interval for automatic updates. (Automatic Update Bookmarks must be enabled.)
+
+## Credits
+
+[Floccus Bookmarks Plugin](https://github.com/mddevils/floccus-bookmarks-plugin) is developed by mddevils.
---
+++ main.ts
@@ -0,0 +1,321 @@
+import { App, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
+import { join, parse } from 'path';
+import { promises as fsPromises } from 'fs';
+import { parseStringPromise } from 'xml2js';
+
+// Remember to rename these classes and interfaces!
+
+interface obfPluginSettings {
+    xbelFolderPath: string;
+    xbelFileName: string;
+    mdFolderPath: string;
+    mdFileName: string;
+    backupFolderPath: string;
+    keepCount: number;
+    automaticUpdate: boolean;
+    updateInterval: number;
+}
+
+const DEFAULT_SETTINGS: obfPluginSettings = {
+    xbelFolderPath: '',
+    xbelFileName: 'bookmarks.xbel',
+    mdFolderPath: '',
+    mdFileName: 'bookmarks.md',
+    backupFolderPath: '',
+    keepCount: 5,
+    automaticUpdate: false,
+    updateInterval: 900,
+}
+
+export default class ofbPlugin extends Plugin {
+    settings: obfPluginSettings;
+
+    async onload() {
+        await this.loadSettings();
+        
+        // This creates an icon in the left ribbon.
+        const bookmarkIconEl = this.addRibbonIcon('bookmark', 'Obsidian Floccus Bookmarks', (evt: MouseEvent) => {
+            // Called when the user clicks the icon.
+            this.processXBELFileData();
+            new Notice('Floccus Bookmarks MD Updated!');
+        });
+
+        // Perform additional things with the ribbon
+        bookmarkIconEl.addClass('my-plugin-ribbon-class');
+
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new FloccusBookmarksSettingTab(this.app, this));
+
+        // Call the processXBELFileData function based on the automatic update setting
+        if (this.settings.automaticUpdate) {
+            const updateInterval = this.settings.updateInterval * 1000; // Convert seconds to milliseconds
+            this.registerInterval(window.setInterval(() => this.processXBELFileData(), updateInterval));
+        }
+
+        // Call the processXBELFileData function
+        this.processXBELFileData();
+    }
+
+    onunload() {}
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+        this.processXBELFileData();
+    }
+
+    async processXBELFileData() {
+        const {
+            xbelFolderPath,
+            xbelFileName,
+            mdFolderPath,
+            mdFileName,
+            backupFolderPath,
+            keepCount,
+        } = this.settings;
+
+        // Construct the full paths
+        const xbelFilePath: string = join(xbelFolderPath, xbelFileName);
+        //const xbelFilePath: string = path.join(xbelFolderPath, xbelFileName);
+        const mdFilePath = `${mdFolderPath}/${mdFileName}`;
+        const mdFile = this.app.vault.getAbstractFileByPath(mdFilePath) as TFile;
+
+        // Create the output folder if it doesn't exist
+        const mdFolder = this.app.vault.getAbstractFileByPath(mdFolderPath) as TFolder;
+        if (!mdFolder) {
+            this.app.vault.createFolder(mdFolderPath);
+        }
+
+        // Check if the output file already exists and backup if necessary
+        if (mdFile) {
+            this.backupExistingFile(mdFile, backupFolderPath);
+        }
+
+        // Delete old backups, keeping only the specified number of most recent ones
+        this.deleteOldBackups(backupFolderPath, keepCount);
+
+        try {
+            // Read the XBEL file
+            const xbelData = await fsPromises.readFile(xbelFilePath, 'utf8');
+        
+            // Parse the XBEL file
+            const result = await parseStringPromise(xbelData);
+        
+            // Generate the folder structure
+            const mdData = this.writeFolderStructure(result.xbel);
+        
+            // Create the Markdown file with the generated data
+            await this.app.vault.create(mdFilePath, mdData);
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    }
+    
+    backupExistingFile(file: TFile, backupFolderPath: string): void {
+        // Generate a date-time suffix in the format 'yyyymmddHHMMSS' using the current timezone
+        const now = new Date();
+        const timeZoneOffset = now.getTimezoneOffset() * 60000; // Convert minutes to milliseconds
+        const localTime = new Date(now.getTime() - timeZoneOffset);
+        const dateSuffix: string = localTime.toISOString().slice(0, 19).replace(/[-T:]/g, '');
+
+        // Create the backup folder if it doesn't exist
+        const backupFolder = this.app.vault.getAbstractFileByPath(backupFolderPath) as TFolder;
+        if (!backupFolder) {
+            this.app.vault.createFolder(backupFolderPath);
+        }
+
+        // Create a new file name with the date-time suffix
+        const fileName = file.basename;
+        const fileExtension = file.extension;
+        const backupFileName = `${parse(fileName).name}-${dateSuffix}.${fileExtension}`;
+        const backupFilePath = `${backupFolderPath}/${backupFileName}`;
+        
+        // Copy the existing file to the backup file
+        this.app.vault.rename(file, backupFilePath);
+        
+    }
+
+    deleteOldBackups(backupFolderPath: string, keepCount: number): void {
+        // Get all files in the backup folder
+        const backupFolder = this.app.vault.getAbstractFileByPath(backupFolderPath) as TFolder;
+        if (!backupFolder) {
+            return;
+        }
+
+        const backupFiles = backupFolder.children as TFile[];
+
+        // Sort the files by modification time in ascending order
+        backupFiles.sort((a, b) => {
+            const statA = a.stat;
+            const statB = b.stat;
+            return statA.mtime - statB.mtime;
+        });
+
+        // Delete files exceeding the keep count
+        const filesToDelete = backupFiles.length - keepCount+1;
+        if (filesToDelete > 0) {
+            const filesToDeleteList = backupFiles.slice(0, filesToDelete);
+            filesToDeleteList.forEach((file) => {
+                this.app.vault.trash(file, false);
+            });
+        }
+    }
+
+    writeFolderStructure(element: any, level = 0): string {
+        let data = '';
+    
+        // Process child elements (folders and bookmarks)
+        if (typeof element === 'object') {
+            if (element.hasOwnProperty('folder') || element.hasOwnProperty('bookmark')) {
+                const folderTitle: string = element.title ? element.title[0] : 'Bookmarks';
+    
+                if (level !== 0) {
+                    data += '\n';
+                }
+    
+                data += '#'.repeat(level+1) + ' ' + folderTitle + '\n';
+            }
+    
+            if (Array.isArray(element.bookmark)) {
+                // Process bookmarks
+                element.bookmark.forEach((bookmark: any) => {
+                    const link: string = bookmark.$.href;
+                    const title: string = bookmark.title[0];
+                    const linkTitle = `[${title}](${link})`;
+                    data += linkTitle + '\n';
+                });
+            }
+    
+            if (Array.isArray(element.folder)) {
+                // Recursively process subfolders
+                element.folder.forEach((subfolder: any) => {
+                    data += this.writeFolderStructure(subfolder, level + 1);
+                });
+            }
+        }
+    
+        return data;
+    }
+    
+}
+
+class FloccusBookmarksSettingTab extends PluginSettingTab {
+    plugin: ofbPlugin;
+
+    constructor(app: App, plugin: ofbPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const { containerEl } = this;
+
+        containerEl.empty();
+        containerEl.createEl('h2', { text: 'Obsidian Floccus Bookmarks Settings' });
+
+        new Setting(containerEl)
+        .setName('XBEL Absolute Folder Path')
+        .setDesc('The absolute folder path of the XBEL file.')
+        .addText((text) =>
+            text
+            .setValue(this.plugin.settings.xbelFolderPath)
+            .onChange(async (value) => {
+                this.plugin.settings.xbelFolderPath = value;
+                await this.plugin.saveSettings();
+            })
+        );
+
+        new Setting(containerEl)
+        .setName('XBEL Filename')
+        .setDesc('The filename of the XBEL file.')
+        .addText((text) =>
+            text
+            .setValue(this.plugin.settings.xbelFileName)
+            .onChange(async (value) => {
+                this.plugin.settings.xbelFileName = value;
+                await this.plugin.saveSettings();
+            })
+        );
+
+        new Setting(containerEl)
+        .setName('MD Vault Folder Path')
+        .setDesc('The vault folder for the generated Markdown file.')
+        .addText((text) =>
+            text
+            .setValue(this.plugin.settings.mdFolderPath)
+            .onChange(async (value) => {
+                this.plugin.settings.mdFolderPath = value;
+                await this.plugin.saveSettings();
+            })
+        );
+
+        new Setting(containerEl)
+        .setName('MD Filename')
+        .setDesc('The filename for the generated Markdown file.')
+        .addText((text) =>
+            text
+            .setValue(this.plugin.settings.mdFileName)
+            .onChange(async (value) => {
+                this.plugin.settings.mdFileName = value;
+                await this.plugin.saveSettings();
+            })
+        );
+
+        new Setting(containerEl)
+        .setName('Backup Folder Path')
+        .setDesc('The vault folder for the backup files.')
+        .addText((text) =>
+            text
+            .setValue(this.plugin.settings.backupFolderPath)
+            .onChange(async (value) => {
+                this.plugin.settings.backupFolderPath = value;
+                await this.plugin.saveSettings();
+            })
+        );
+
+        new Setting(containerEl)
+        .setName('Number of Backups to Keep')
+        .setDesc('The number of backup files to keep.')
+        .addText((text) =>
+            text
+            .setValue(String(this.plugin.settings.keepCount))
+            .onChange(async (value) => {
+                const keepCount = parseInt(value, 10);
+                if (!isNaN(keepCount)) {
+                    this.plugin.settings.keepCount = keepCount;
+                    await this.plugin.saveSettings();
+                }
+            })
+        );
+
+        new Setting(containerEl)
+        .setName('Automatic Update Bookmarks')
+        .setDesc('Enable automatic updating of bookmarks.')
+        .addToggle((toggle) =>
+            toggle
+            .setValue(this.plugin.settings.automaticUpdate)
+            .onChange(async (value) => {
+                this.plugin.settings.automaticUpdate = value;
+                await this.plugin.saveSettings();
+            })
+        );
+
+        new Setting(containerEl)
+        .setName('Update Interval (in seconds)')
+        .setDesc('Specify the interval for automatic updates. Automatic Update Bookmarks must be on.')
+        .addText((text) =>
+            text
+            .setValue(String(this.plugin.settings.updateInterval))
+            .onChange(async (value) => {
+                const updateInterval = parseInt(value, 10);
+                if (!isNaN(updateInterval)) {
+                    this.plugin.settings.updateInterval = updateInterval;
+                    await this.plugin.saveSettings();
+                }
+            })
+        );
+    }
+}
